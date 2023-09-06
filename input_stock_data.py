a20f2c5cc283d56fd4fd2ccc84e33208accaef5f
TobiS <130932230+NixTS@users.noreply.github.com>
2023-09-06 22:08:52 +0200

    Add line break function

--- input_stock_data.py
+++ input_stock_data.py
@@ -29,6 +29,8 @@ datetoday = today.strftime("%d.%m.%Y")
 
 trading_journal_entry = []
 
+def line_break():
+    print("__________________________________________________\n")
 
 
 def handle_input_date():
@@ -37,14 +39,16 @@ def handle_input_date():
     the current date should be used or
     if one wants to input a date manually.
     """
-    print(f"Today is the {datetoday}. If you want to enter this date:\n")
-    print("press '1' on your keyboard.")
+    line_break()
+    print("Please input your stock trading data by following the instructions given to you.\n")
+    print(f"Today is the {datetoday}. Do you want to enter this date?\n")
+    print("press '1' on your keyboard to use the current date.")
     print("Or:")
     print("press '2', if you want to enter a date manually.\n")
 
     
     while True:
-        choice = input("Enter your choice:")
+        choice = input("Enter your choice: ")
         if choice == "1":
             input_date_today()
             break
@@ -52,6 +56,7 @@ def handle_input_date():
             input_date_manually()
             break
         else:
+            line_break()
             print("Invalid format, please enter '1' or '2'.")
 
 
@@ -60,6 +65,7 @@ def input_date_today():
     If option 1 is selected, prints a message showing the current date
     and append the date to the list.
     """
+    line_break()
     print(f"The current date '{datetoday}' has been parsed to your journal.")
 
     trading_journal_entry.append(datetoday)
@@ -70,12 +76,15 @@ def input_date_manually():
     Enter a date in the format DD.MM.YYYY. 
     Prints a ValueError to the terminal if the wrong format is used, input repeats.
     """
+    line_break()
     date_str = input("Enter a date (DD.MM.YYYY): ")
 
     try:
         date_obj = datetime.strptime(date_str, "%d.%m.%Y")
         print(f"Your input '{datetoday}' has been parsed to your journal.")
     except ValueError:
+        line_break()
+        print(ValueError)
         print("Invalid date format, please use DD.MM.YYYY format.")
     
     trading_journal_entry.append(date_str)
@@ -89,11 +98,13 @@ def input_ticker():
     The ticker will be appended to the list.
     """
     while True:
+        line_break()
         ticker = input("Enter ticker: ")
 
         if re.match(r"^[a-zA-Z]{1,4}$", ticker):
             break
         else:
+            line_break()
             print("Invalid input, please enter 1 to 4 letters.")
 
     trading_journal_entry.append(ticker.upper())
@@ -106,11 +117,13 @@ def input_shares_amount():
     The shares amount will be appended to the list.
     """
     while True:
+        line_break()
         shares_amount = input("Shares amount traded: ")
 
         if re.match(r"^([\s\d]+)$", shares_amount):
             break
         else:
+            line_break()
             print("Invalid input, please enter a number.")
 
     trading_journal_entry.append(shares_amount)
@@ -123,10 +136,11 @@ def input_direction():
     Loop repeats if input was incorrect.
     The word Long/Short will be appended to the list.
     """
-    print("For a long trade please enter 'L'.")
-    print("For a short short please enter 'S'.")
 
     while True:
+        line_break()
+        print("For a long trade please enter 'L'.\n")
+        print("For a short short please enter 'S'.\n\n")
         direction = input("Enter trade direction: ")
 
         if re.match(r"^[lL]$", direction):
@@ -136,6 +150,7 @@ def input_direction():
             trading_journal_entry.append("Short")        
             break
         else:
+            line_break()
             print("Invalid input, please enter 'L' for Long, and 'S' for a short trade.")
 
 
@@ -146,17 +161,19 @@ def input_prices():
     Loop repeats if input was incorrect.
     The shares amount will be appended to the list.
     """
-    print("Enter your entry and exit prices.")
 
     prices = []
 
     while True:
+        line_break()
+        print("Enter your entry and exit prices.\n")
         entry_price = input("Enter your trade entry price: ")
 
         if re.match(r"^([\s\d]+)$", entry_price):
             prices.append(entry_price)
             break
         else:
+            line_break()
             print("Invalid input, please enter a number.")
 
     while True:
@@ -176,7 +193,8 @@ def show_input():
     After successful input of all entries and overview
     in form of a table will be displayed.
     """
-    print("This is the data you put in:")
+    line_break()
+    print("This is the data you put in:\n")
 
     table = [["Date:", trading_journal_entry[0]],["Ticker:", trading_journal_entry[1]],["Amount of Shares:", trading_journal_entry[2]],["Direction:", trading_journal_entry[3]],["Entry Price:", "$ " + trading_journal_entry[4]],["Exit Price:", "$ " + trading_journal_entry[5]]]
     print(tabulate(table))
@@ -191,17 +209,20 @@ def push_input_to_sheet():
     If not, process repeats.
     """
     while True:
+        print("\n")
         push_data = input("If this is correct, type 'Y', if you want to restart type 'N'.")
 
         if re.match(r"^[yY]$", push_data):
+            line_break()
             print("Pushing to journal . . .\n")
             stock_data = SHEET.worksheet('stock_data')
             stock_data.append_row(trading_journal_entry)
-            time.sleep(1)
+            time.sleep(2)
             print("Push successful! Your entry is now stored in your trading journal!\n")
-            time.sleep(1)
+            time.sleep(2)
             print("Restarting process . . .\n")
-            time.sleep(1)
+            print("This takes only 5 seconds . . .")
+            time.sleep(5)
             main()
             break
         elif re.match(r"^[nN]$", push_data):
@@ -209,6 +230,7 @@ def push_input_to_sheet():
             main()
             break
         else:
+            line_break()
             print("Invalid input, please enter 'Y' to push entry, or 'N' to start over.")
 
     
@@ -224,5 +246,4 @@ def main():
     show_input()
 
 
-print("Please input your stock trading data by following the instructions given to you.\n")
 main()
