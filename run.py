26eec08cbc0fd8a43bc0c9901601466b60929b12
TobiS <130932230+NixTS@users.noreply.github.com>
2023-09-06 13:45:34 +0200

    Wire up APIs and confirm accres to google sheet

--- run.py
+++ run.py
@@ -1,3 +1,23 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import gspread
+
+from google.oauth2.service_account import Credentials
+
+from colorama import just_fix_windows_console
+just_fix_windows_console()
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('trading-journal')
+
+stock_data = SHEET.worksheet('stock_data')
+
+data = stock_data.get_all_values()
+
+print(data)
