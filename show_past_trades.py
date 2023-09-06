4793c99d76ec00e5ea99b1492b5977fe0c49e1e6
TobiS <130932230+NixTS@users.noreply.github.com>
2023-09-06 22:39:19 +0200

    Add imports and creds to show_past_trades.py

--- show_past_trades.py
+++ show_past_trades.py
@@ -1,4 +1,35 @@
-def file_2_test():
-    print("show_past_trades.py is ready")
+from tabulate import tabulate
 
-file_2_test()
+import re
+
+import gspread
+from google.oauth2.service_account import Credentials
+
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('trading-journal')
+
+stock_data = SHEET.worksheet('stock_data')
+
+data = stock_data.get_all_values()
+
+
+def line_break():
+    print("__________________________________________________\n")
+
+
+def main():
+    """
+    Run all program functions
+    """
+
+
+main()
