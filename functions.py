f267f26fe3ecc0cf13e9247ebd4d03c1557b66e7
TobiS <130932230+NixTS@users.noreply.github.com>
2023-09-07 17:16:37 +0200

    Add google import to sheet_data.py and import in other files

---
+++ functions.py
@@ -0,0 +1,9 @@
+def line_break():
+    """
+    Adds a line and a blank line underneath.
+    For visual pleasure.
+    """
+    print("__________________________________________________\n")
+
+import sys
+print(sys.path)
--- run.py
+++ run.py
@@ -1,27 +1,10 @@
-import gspread
-
-from google.oauth2.service_account import Credentials
+from functions import line_break
+from sheet_data import *
 
 import subprocess
 
 from colorama import just_fix_windows_console
 just_fix_windows_console()
-
-SCOPE = [
-    "https://www.googleapis.com/auth/spreadsheets",
-    "https://www.googleapis.com/auth/drive.file",
-    "https://www.googleapis.com/auth/drive"
-    ]
-
-CREDS = Credentials.from_service_account_file('creds.json')
-SCOPED_CREDS = CREDS.with_scopes(SCOPE)
-GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open('trading-journal')
-
-stock_data = SHEET.worksheet('stock_data')
-
-data = stock_data.get_all_values()
-
 
 def start():
     """
