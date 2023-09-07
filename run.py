8e73d47f9feabf6e79a4d40ad1903ea84cb911b6
TobiS <130932230+NixTS@users.noreply.github.com>
2023-09-07 22:13:51 +0200

    Edit run.py print messages and docstrings

--- run.py
+++ run.py
@@ -14,18 +14,23 @@ def start():
     """
     Starting point
     Prints a message to the UI to select options:
-    1: Input stock trading data
-    2: Show past trades
-    3: Get statistics 
     """
-
-    print("Welcome to your Stock Trading Journal")
+    line_break()
+    print("Welcome to your Stock Trading Journal!\n")
     print("Please press one of the following buttons on your keyboard to continue:\n")
     print("'1' to input your stock trading data.")
-    print("'2' to see your past journal entries.")
-    print("'3' to see your past trading statistics.\n")
+    print("'2' to display your journal entries.")
+    print("'3' to display your trading statistics.\n")
+    print("'ESC' to exit the program.")
 
 def on_key_press(key):
+    """
+    Handles key presses to 
+    1: Input stock trading data to journal
+    2: Display trading journal
+    3: Display trading statistics 
+    ESC: to exit the program
+    """
     try:
         if key.char in allowed_keys:
             if key.char == '1':
@@ -38,10 +43,10 @@ def on_key_press(key):
         # Handle 'Esc' key
         if key == keyboard.Key.esc:
             print("\nExiting the program.")
+            line_break()
             quit()
 
 start()
 
 with Listener(on_press=on_key_press) as listener:
-    print("Press 1, 2, or 3 to select an option (Esc to exit).")
     listener.join()
